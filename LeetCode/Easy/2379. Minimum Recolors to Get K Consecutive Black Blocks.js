36d979abd734c043075814a57fef0b36eb79f09c
LeeJinyoung218 <amy021899@gmail.com>
2025-03-08 22:47:01 +0900

    122일차

---
+++ LeetCode/Easy/2379. Minimum Recolors to Get K Consecutive Black Blocks.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+var minimumRecolors = function(blocks, k) {
+    let blackCount = 0, ans = blocks.length;
+    
+    for (let i = 0; i < blocks.length; i++) {
+        if (i - k >= 0 && blocks[i - k] === 'B') blackCount--;
+        if (blocks[i] === 'B') blackCount++;
+        ans = Math.min(ans, k - blackCount);
+    }
+    
+    return ans;
+};
